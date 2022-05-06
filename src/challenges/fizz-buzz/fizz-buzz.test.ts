7fb08e6a05370e85138e162fc3ebb7c1de999b49
Alexander Badak <badak@sgs-media.de>
2022-05-06 10:27:39 +0200

    Add Explanations

--- src/challenges/fizz-buzz/fizz-buzz.test.ts
+++ src/challenges/fizz-buzz/fizz-buzz.test.ts
@@ -1,5 +1,18 @@
 import { fizzBuzzV1 as fizzBuzz } from './fizz-buzz'
 
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
 describe('Fizz Buzz', () => {
     test('', () => {
         const expected = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
