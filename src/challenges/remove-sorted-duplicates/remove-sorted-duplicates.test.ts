132c4eec6c27d9516b971b4606eb1e648782e8a8
Alexander Badak <alexander.badak@onefor.com>
2022-06-03 10:51:38 +0200

    Add Description and another test

--- src/challenges/remove-sorted-duplicates/remove-sorted-duplicates.test.ts
+++ src/challenges/remove-sorted-duplicates/remove-sorted-duplicates.test.ts
@@ -1,7 +1,17 @@
 import { removeSortedDuplicatesV1 as removeSortedDuplicates } from './remove-sorted-duplicates'
 
+// create a method with one parameter which is a sorted array of integers
+// move the duplicate numbers to the end of the array
+// the size of the array must not change, therefore the last digits are to be ignored
+// a new array may not be created
+// return the length of the new array
+
 describe('Remove Sorted Duplicates', () => {
     test('should return the array length', () => {
         expect(removeSortedDuplicates([1, 2, 2, 2, 3, 6, 6, 8])).toEqual(5)
+    })
+
+    test('', () => {
+        expect(removeSortedDuplicates([1, 1, 1, 4])).toEqual(2)
     })
 })
