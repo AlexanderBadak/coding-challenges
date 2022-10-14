0e7bac5f34fa95086493d5d91c2053dc456d738b
Alexander Badak <badak@sgs-media.de>
2022-10-14 10:08:42 +0200

    Tests

--- src/challenges/missing-positive/missing-positive.test.ts
+++ src/challenges/missing-positive/missing-positive.test.ts
@@ -35,4 +35,13 @@ describe('first missing positive', () => {
     test('case 5', () => {
         expect(missingPositiveV1([-1, 1, 3, 6, 7, 9, 10, 11, 2, 4, 6, 5, 8, 13])).toBe(12)
     })
+    test('case 6', () => {
+        expect(missingPositiveV1([1, 2, 3, 4, 5, 6, 7])).toBe(8)
+    })
+    test('case 7', () => {
+        expect(missingPositiveV1([1, 2, 17, 18])).toBe(3)
+    })
+    test('case 8', () => {
+        expect(missingPositiveV1([4, 3, 2, 1])).toBe(5)
+    })
 })
--- src/challenges/two-sum/two-sum.test.ts
+++ src/challenges/two-sum/two-sum.test.ts
@@ -1,4 +1,4 @@
-import { twoSumV1 as twoSum } from './two-sum'
+import { twoSumV2 as twoSum } from './two-sum'
 
 // Write a function that takes an array of numbers (integers for the tests)
 // and a target number. It should find two different items in the array that,
@@ -14,12 +14,13 @@ describe('Two Sum', () => {
     test('case 1', () => {
         expect(twoSum([1, 2, 3, 4], 4)).toStrictEqual([0, 2])
     })
-
     test('case 2', () => {
         expect(twoSum([1234, 5678, 9012], 14690)).toStrictEqual([1, 2])
     })
-
     test('case 3', () => {
         expect(twoSum([2, 2, 3], 4)).toStrictEqual([0, 1])
+    })
+    test('case 4', () => {
+        expect(twoSum([3, 2, 2, 3], 4)).toStrictEqual([1, 2])
     })
 })
