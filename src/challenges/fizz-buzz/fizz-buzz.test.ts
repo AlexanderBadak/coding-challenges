59f34c810853050e6c34d603e1e2da90fab9510a
Alexander Badak <>
2023-02-08 13:11:39 +0100

    F

--- src/challenges/fizz-buzz/fizz-buzz.test.ts
+++ src/challenges/fizz-buzz/fizz-buzz.test.ts
@@ -9,13 +9,13 @@ import { fizzBuzzV1 as fizzBuzz } from './fizz-buzz'
 // If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
 // N will never be less than 1.
 
-// Method calling example:
-
-// fizzbuzz(3) -->  [1, 2, "Fizz"]
-
 describe('Fizz Buzz', () => {
     test('case 1', () => {
         const expected = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
         expect(fizzBuzz(15)).toStrictEqual(expected)
+    })
+    test('case 2', () => {
+        const expected = [1, 2, 'Fizz']
+        expect(fizzBuzz(3)).toStrictEqual(expected)
     })
 })
