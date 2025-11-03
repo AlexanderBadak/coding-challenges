faa8997d3e08fa60bd2539d564e53ee86e910123
Alexander Badak <alexander.badak@onefor.com>
2025-11-03 12:15:24 +0100

    uncompressed strings

---
+++ src/challenges/uncompress-string/uncompress-string.test.ts
@@ -0,0 +1,30 @@
+// Given a compressed string, return its original form.
+
+// For example.
+// uncompress('3(ab)') // 'ababab'
+// uncompress('3(ab2(c))') // 'abccabccabcc'
+
+// a number k followed by a pair of parenthesis, meaning to repeat the substring
+// inside the parenthesis by k times, k is positive integer.
+// inputs are guaranteed to be valid input like above example, 
+// there is no numerical digit in original form.
+
+import { uncompressStringV1 as uncompressString} from "./uncompress-string"
+
+describe('uncompress string', () => {
+    const testParams = [
+        ['3(ab)', 'ababab'],
+        ['3(ab2(c))', 'abccabccabcc'],
+        ['2(a)2(b)', 'aabb'],
+        ['gg3(ab)', 'ggababab'],
+        ['10(a)', 'aaaaaaaaaa'],
+        ['11(b)', 'bbbbbbbbbbb'],
+        ['3(b)a', 'bbba'],
+        ['2(a2(b))2(c)', 'abbabbcc'],
+        ['3(ab3(cd))3(ef)', 'abcdcdcdabcdcdcdabcdcdcdefefef'],
+        ['3(ab2(c))3(ab)', 'abccabccabccababab'],
+    ] as const
+    test.each(testParams)('cases', (iteration, result) => {
+        expect(uncompressString(iteration)).toEqual(result)
+    })
+})
---
+++ src/challenges/uncompress-string/uncompress-string.ts
@@ -0,0 +1,94 @@
+// export const uncompressStringRecursionFailedV1 = (s: string) => {
+//     let finalStr = ''
+//     let currentNr = ''
+//     for (let i = 0; i < s.length; i++) {
+//         const currentChar = s[i]
+//         if (currentChar === '(') {
+//             const finalNr = parseInt(currentNr)
+//             const remainingStr = s.substring(i + 1)
+//             const result = uncompressStringV1(remainingStr)
+//             finalStr += result.repeat(finalNr)
+//             currentNr = ''
+//             const skipIndexCount = result.length + 1 // + 1 because we know a ) is at the end
+//             i += skipIndexCount
+//             // const nextChar = s[i + 1] // + 1 becuase i++ hasnt triggered yet
+//             // if (nextChar === ')') {
+//             //     i++
+//             // }
+//         } else if (numbersStrObj[currentChar] !== undefined) {
+//             currentNr += numbersStrObj[currentChar].toString()
+//         } else if (currentChar === ')') {
+//             return finalStr
+//         } else {
+//             finalStr += currentChar
+//         }
+//     }
+
+//     return finalStr
+// }
+
+const numbersStrObj: Record<string, number> = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9
+}
+
+export const uncompressStringV1 = (s: string) => {
+    let finalStr = ''
+    let depth = 0
+
+    const depthKVP: Record<number, { currentStr: string, multiplier: number}> = {
+        0: {
+            multiplier: -1,
+            currentStr: ''
+        }
+    }
+
+    let currentNrTemp = ''
+    
+    for (let i = 0; i < s.length; i++) {
+        const currentChar = s[i]
+
+        if (numbersStrObj[currentChar] !== undefined) {
+            currentNrTemp += numbersStrObj[currentChar].toString()
+        } else if (currentChar === '(') {
+            depth++
+            const multiplier = parseInt(currentNrTemp)
+
+            depthKVP[depth] = {
+                multiplier,
+                currentStr: ''
+            }
+            currentNrTemp = ''
+        } else if (currentChar === ')') {
+            let currentStr = ''
+            for (; depth > 0; depth--) {
+                const depthStr = `${depthKVP[depth].currentStr}${currentStr}`.repeat(depthKVP[depth].multiplier)
+                if (depth === 1) {
+                    currentStr = ''
+                }
+                currentStr = `${depthStr}${currentStr}`
+                depthKVP[depth] = {
+                    multiplier: -1,
+                    currentStr: ''
+                }
+            }
+            finalStr += currentStr
+        } else {
+            if (depth > 0) {
+                depthKVP[depth].currentStr += currentChar 
+            } else {
+                finalStr += currentChar
+            }
+        }
+    }
+
+    return finalStr
+}
